491224ab74c69bd6c3409a9bd1569d62f9589e42
toughskg <blueskk@gmail.com>
2017-02-11 17:01:03 +0900

    init

---
+++ myWvm/src/myWvm/m.java
@@ -0,0 +1,27 @@
+/**
+ * 
+ */
+package myWvm;
+
+/**
+ * @author toughskg
+ *
+ */
+public class m {
+
+	/**
+	 * 
+	 */
+	public m() {
+		// TODO Auto-generated constructor stub
+	}
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
