890d15214755a2317e31297154e74f6506e8bafd
KWANG KYUN SHIN <blueskk@gmail.com>
2021-06-01 14:22:17 +0900

    no message

---
+++ shsoft/src/main/java/com/shsoft/ShsoftApplication.java
@@ -0,0 +1,13 @@
+package com.shsoft;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShsoftApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ShsoftApplication.class, args);
+	}
+
+}
---
+++ shsoft/src/test/java/com/shsoft/ShsoftApplicationTests.java
@@ -0,0 +1,13 @@
+package com.shsoft;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShsoftApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
